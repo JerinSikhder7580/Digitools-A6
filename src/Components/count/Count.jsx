14fd0899e7f92adb76b01dd861133ca8afd5636d
JerinSikhder7580 <jerinahmed615@gmail.com>
2026-03-31 17:45:29 +0600

    Count

--- src/Components/count/Count.jsx
+++ src/Components/count/Count.jsx
@@ -9,6 +9,7 @@ const Count = () => {
                         <h1 className='text-4xl font-extrabold text-white mb-5'>50K+</h1>
                         <p className='text-white'>Active Users</p>
 
+
                     </div>
                     <div>
                         <h1 className='text-4xl font-extrabold text-white mb-5'>200+</h1>
