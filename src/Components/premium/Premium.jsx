6c2e4a6a12513c4d3552b9282a321091aef907c5
JerinSikhder7580 <jerinahmed615@gmail.com>
2026-03-31 17:48:49 +0600

    Premium

--- src/Components/premium/Premium.jsx
+++ src/Components/premium/Premium.jsx
@@ -9,7 +9,7 @@ const Premium = ({cartData, activeButton, setActiveButton}) => {
             <section>
                 <div className='p-8  text-center space-y-4'>
                     <h1 className='text-3xl font-bold'>Premium Digital Tools</h1>
-                    <p className='text-gray-500 text-sm'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
+                    <p className='text-gray-500 text-sm'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity..</p>
                     <div className='border border-gray-300 w-max mx-auto p-1 rounded-full flex gap-2'>
                         <button
                             onClick={() => setActiveButton("products")}
